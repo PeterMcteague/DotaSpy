6ef609670149a0cc89b4797d01e0673ad2be348c
HellkatAnarchy <07mcteaguepet@googlemail.com>
2016-02-25 18:33:34 +0000

    Updated to version 2.0
    V2.0
    -Updated readme.txt
    -Updated changelog.txt
    -Added an icon for the application
    -Named project dotaspy
    -Added a banner to the setup screen
    -Stopped profiles from displaying if an invalid url is entered
    -Hid hero icons of private profiles instead of displaying defaults.
    -Added the number of times the top 3 heroes have been played in X games
    under the image.
    -Fixed a bug where private profiles showed hero pictures (From the
    unsorted list) and displayed role.
    -Fixed a bug where players who did not play dota would be reported as
    having private profiles.
    -Pressing enter now submits the profile.
    -Made displaying of hero information more efficient (Now only attempts
    to do so when appropriate)
    -Made various visual improvements to main window

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import webbrowser
 import urllib.request
 import json
 import io
-import sys
 from PIL import Image, ImageTk
 
 class SetupWindow:
@@ -23,7 +22,9 @@ class SetupWindow:
             apikeyFile.write(self.apiKey.get())
             apikeyFile.close()
             self.SetupWindow.destroy()
-            self.CurrentWindow = MainWindow()
+            #Make sure currentwindow is given global scope, as code was written for it to be at the top level.
+            global CurrentWindow
+            CurrentWindow = MainWindow()
         else:
             tk.messagebox.showinfo("Invalid API Key", "The API Key you've entered is invalid. Try again.")
             
@@ -35,27 +36,32 @@ class SetupWindow:
         #Graphics variables initialisation
         self.WindowBackgroundColor = "#bdc3c7"
         self.WindowWidth = "600"
-        self.WindowHeight = "200"
+        self.WindowHeight = "300"
         self.SetupFont = 'Times New Roman'
         self.SetupWindow = tk.Tk()
+        self.img = ImageTk.PhotoImage(file='images/dotaspyicon.png')
+        self.SetupWindow.tk.call('wm', 'iconphoto', self.SetupWindow, self.img)
         self.SetupWindow.resizable(width="FALSE", height="FALSE")
         self.SetupWindow.attributes("-fullscreen", False)
-        self.SetupWindow.title('Dota 2 Player Information Getter - First time setup')
+        self.SetupWindow.title('DotaSpy - First time setup')
         self.SetupWindow.configure(background=self.WindowBackgroundColor)
         #Setting the window size and placing it to 0.35*Screen width and 0.3*Screen height. Had to cast to int twice to get rid of decimal points.
-        self.SetupWindow.geometry(self.WindowWidth + "x" + self.WindowHeight + "+" + str(int(int(self.SetupWindow.winfo_screenwidth()) * 0.35)) + "+" + str(int(int(self.SetupWindow.winfo_screenheight() * 0.3)))) 
+        self.SetupWindow.geometry(self.WindowWidth + "x" + self.WindowHeight + "+" + str(int(int(self.SetupWindow.winfo_screenwidth()) * 0.35)) + "+" + str(int(int(self.SetupWindow.winfo_screenheight() * 0.3))))
+        #Banner image
+        self.banner = ImageTk.PhotoImage(file='images/dotaspybanner.png')
+        tk.Label(self.SetupWindow, background = self.WindowBackgroundColor, image = self.banner, width=400, height=100).place(x=100, y=20)
         #Window variable initialisation
         self.apiKey = tk.StringVar()
         tk.Label(self.SetupWindow, text = "Setup"
-                 , font = (self.SetupFont,16), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=0)
+                 , font = (self.SetupFont,16), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=150)
         tk.Label(self.SetupWindow, text = "  An API key is required to pull information from Dota 2 matches."
-                 , font = (self.SetupFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=50, y=30)
+                 , font = (self.SetupFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=50, y=180)
         self.APIKeyLabel = tk.Label(self.SetupWindow, text = "Please enter an API key from http://steamcommunity.com/dev/apikey."
                  , font = (self.SetupFont,14), background = self.WindowBackgroundColor, fg="#ffffff")
-        self.APIKeyLabel.place(x=50, y=55)
+        self.APIKeyLabel.place(x=50, y=205)
         self.APIKeyLabel.bind("<Button-1>", self.openAPILink)
-        self.APIKeyEntry = tk.Entry(self.SetupWindow, bd = 0, font = (self.SetupFont,12), exportselection=0, textvariable = self.apiKey, width=40).place(x=125,y=105)
-        tk.Button(self.SetupWindow, text ="Okay", command = self.checkKey, bg= "#95a5a6", relief='flat').place(x=450,y=102)
+        self.APIKeyEntry = tk.Entry(self.SetupWindow, bd = 0, font = (self.SetupFont,12), exportselection=0, textvariable = self.apiKey, width=40).place(x=125,y=255)
+        tk.Button(self.SetupWindow, text ="Okay", command = self.checkKey, bg= "#95a5a6", relief='flat').place(x=450,y=252)
 
 #Unfinished. Code has only been ported from V1.
 class PlayerProfile():
@@ -134,7 +140,7 @@ class PlayerProfile():
                             #Increments timesplayed
                             self.HeroList[int(MatchHistory["result"]["matches"][MatchNumber]["players"][PlayerPosition]["hero_id"]) - 1][2] += 1  
         else:
-            tk.messagebox.showinfo("Error 15", "Cannot get match history for a user that hasn't allowed it.")
+            tk.messagebox.showinfo("Error 15", "Error, could not get DOTA info.")
     def resetHeroList(self):
         for item in self.HeroList:
             item[2] = 0
@@ -153,13 +159,15 @@ class PlayerProfile():
         self.HeroList = heroList
 
 class MainWindow:
+    def key(self, event):
+        self.getData()
+    
     def setupKey(self):
         self.MainWindow.destroy()
         os.remove("apikey.txt")
         SetupWindow()
         
     def getData(self):
-            
         #Only allow data to be gotten if MatchesToGet < 100 > 0 and type int.
         try:
             if 100 >= self.MatchesToGet.get() > 0:
@@ -167,6 +175,11 @@ class MainWindow:
                 self.PlayerProfile = PlayerProfile()
                 #Run object methods and set graphics on MainWindow
                 if self.PlayerProfile.fetchSteamID() != "error":
+                    #Get rid of error label
+                    self.ErrorLabel = tk.Label(self.MainWindow, text = ""
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff", width = 80, height = 6).place(x=0, y=50)
+                    self.ErrorLabel2 = tk.Label(self.MainWindow, text = ""
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff", width = 71, height = 5).place(x=0, y=180)   
                     self.PlayerProfile.fetchSteamUserJson()
                     self.PlayerProfile.fetchSteamName()
                     self.PlayerProfile.fetchSteamNationality()
@@ -176,7 +189,7 @@ class MainWindow:
                     #Add info gotten to UI
                     #Display SteamID under entry
                     tk.Label(self.MainWindow, text = "Steam ID: " + self.PlayerProfile.SteamID
-                             , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=40)
+                             , font = (self.MainFont,12), background = self.WindowBackgroundColor, fg="#ffffff", height = 1).place(x=0, y=50)
                     #Display steam avatar
                     tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.PlayerProfile.SteamAvatar, width=184, height=184).place(x=0, y=80)
                     #Display steam name and nationality
@@ -189,42 +202,63 @@ class MainWindow:
                     elif self.PlayerProfile.SupportCount > self.PlayerProfile.CoreCount:
                         self.RoleLabel = tk.Label(self.MainWindow, text = "Support                                                               "
                              , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=110)
-                    elif self.PlayerProfile.CoreCount == self.PlayerProfile.SupportCount:
-                        self.RoleLabel = tk.Label(self.MainWindow, text = "In your last X matches, you've played support and core heroes equally."
+                    elif self.PlayerProfile.CoreCount == self.PlayerProfile.SupportCount == 0:
+                        self.RoleLabel = tk.Label(self.MainWindow, text = str(self.PlayerProfile.SteamName) + " either has a private profile or does not play Dota."
                              , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=110)
-                    #Display 3 most played heroes
+                    elif self.PlayerProfile.CoreCount == self.PlayerProfile.SupportCount:
+                        self.RoleLabel = tk.Label(self.MainWindow, text = "In this players last " + self.MatchesToGet.get() +  " matches, they've played support and core heroes equally."
+                             , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=110)
                     #Sort hero list by played
                     self.PlayerProfile.HeroList = sorted(self.PlayerProfile.HeroList, key=lambda timesPlayed: timesPlayed[2])
+                    
                     #Display
-                    #Destroy previously displayed
-                    try:
-                        self.HeroPlayedLabel.destroy()
-                    except AttributeError:
-                        pass                                        
+                    #Display the last heroes played message                                    
                     if self.MatchesToGet.get() == 1:
                         tk.Label(self.MainWindow, text = "Last hero played:               "
-                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=300)
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=160)
                     elif self.MatchesToGet.get() == 2: 
                         tk.Label(self.MainWindow, text = "Last 2 heroes played:           "
-                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=300)
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=160)
                     elif self.MatchesToGet.get() == 3:
                         tk.Label(self.MainWindow, text = "Last 3 heroes played:           "
-                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=300)
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=160)
                     else:
                         tk.Label(self.MainWindow, text = "Top 3 heroes played:            "
-                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=300)
-                    self.heroPhoto1 = ImageTk.PhotoImage(file="images/" + str(self.PlayerProfile.HeroList[-1][0]) + ".png")
-                    self.heroPhoto2 = ImageTk.PhotoImage(file="images/" + str(self.PlayerProfile.HeroList[-2][0]) + ".png")
-                    self.heroPhoto3 = ImageTk.PhotoImage(file="images/" + str(self.PlayerProfile.HeroList[-3][0]) + ".png")
-                    tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.heroPhoto1, width=80, height=45).place(x=0, y=330)
-                    tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.heroPhoto2, width=80, height=45).place(x=80, y=330)
-                    tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.heroPhoto3, width=80, height=45).place(x=160, y=330)
-                    if self.MatchesToGet.get() == 1:
-                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, width=80, height=45).place(x=80, y=330)
-                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, width=80, height=45).place(x=160, y=330)
-                    elif self.MatchesToGet.get() == 2:  
-                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, width=80, height=45).place(x=160, y=330)
-                    
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=200, y=160)
+
+                    #Here we display the appropriate number of hero images and played count for the contents of the hero list.
+                    #If no heroes played
+                    if self.PlayerProfile.HeroList[-1][2] == 0:
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, width=12, height=7).place(x=0, y=300)
+                    else:
+                        #Display a thematic break label
+                        self.seperator = ImageTk.PhotoImage(file="images/seperator.png")
+                        tk.Label(self.MainWindow, background = "#ffffff", fg = "#ffffff", image = self.seperator, width=580, height= 1).place(x=200, y=145)
+                        #Display photo and times played
+                        self.heroPhoto1 = ImageTk.PhotoImage(file="images/" + str(self.PlayerProfile.HeroList[-1][0]) + ".png")
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.heroPhoto1, width=80, height=45).place(x=200, y=190)
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, text = str(self.PlayerProfile.HeroList[-1][2]),font = (self.MainFont,14), width=1, height=1, fg="#ffffff").place(x=240, y=240)
+                    #If one hero played
+                    if self.PlayerProfile.HeroList[-2][2] == 0:
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, width=12, height=7).place(x=80, y=310)
+                    else:
+                        self.heroPhoto2 = ImageTk.PhotoImage(file="images/" + str(self.PlayerProfile.HeroList[-2][0]) + ".png")
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.heroPhoto2, width=80, height=45).place(x=280, y=190)
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, text = str(self.PlayerProfile.HeroList[-2][2]),font = (self.MainFont,14), width=1, height=1, fg="#ffffff").place(x=320, y=240)
+                    #If two heroes played
+                    if self.PlayerProfile.HeroList[-3][2] == 0:
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, width=12, height=7).place(x=160, y=310)
+                    else:
+                        self.heroPhoto3 = ImageTk.PhotoImage(file="images/" + str(self.PlayerProfile.HeroList[-3][0]) + ".png")
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, image = self.heroPhoto3, width=80, height=45).place(x=360, y=190)
+                        tk.Label(self.MainWindow, background = self.WindowBackgroundColor, text = str(self.PlayerProfile.HeroList[-3][2]),font = (self.MainFont,14), width=1, height=1, fg="#ffffff").place(x=400, y=240)
+                        
+                else:
+                    #Cover the info up
+                    self.ErrorLabel = tk.Label(self.MainWindow, text = "Error, profile not found or invalid."
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff", width = 80, height = 6).place(x=0, y=50)
+                    self.ErrorLabel2 = tk.Label(self.MainWindow, text = " "
+                                 , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff", width = 71, height = 5).place(x=0, y=180)  
             else:
                 tk.messagebox.showinfo("Error", "The number of games to scan must be in the range 1-100.")
                 return    
@@ -240,27 +274,39 @@ class MainWindow:
         #Create UI
         self.WindowBackgroundColor = "#bdc3c7"
         self.WindowWidth = "800"
-        self.WindowHeight = "800"
+        self.WindowHeight = "265"
         self.MainFont = 'Times New Roman'
         self.MainWindow = tk.Tk()
+        self.MainWindow.bind("<Return>", self.key)
+        self.img = ImageTk.PhotoImage(file='images/dotaspyicon.png')
+        self.MainWindow.tk.call('wm', 'iconphoto', self.MainWindow, self.img)
         self.MainWindow.resizable(width="FALSE", height="FALSE")
         self.MainWindow.attributes("-fullscreen", False)
-        self.MainWindow.title('Dota 2 Player Information Getter')
+        self.MainWindow.title('DotaSpy')
         self.MainWindow.configure(background=self.WindowBackgroundColor)
         self.MainWindow.geometry(self.WindowWidth + "x" + self.WindowHeight + "+" + str(int(int(self.MainWindow.winfo_screenwidth()) * 0.35)) + "+" + str(int(int(self.MainWindow.winfo_screenheight() * 0.1)))) 
         #Create button that links back to setup
-        tk.Button(self.MainWindow, text ="Setup", command= self.setupKey, bg= "#95a5a6", relief='flat', height = 5, width = 10).place(x=720,y=720)
-        #Create button and box for steamID
-        self.steamURL = tk.StringVar()
+        tk.Button(self.MainWindow, text ="Setup", command= self.setupKey, bg= "#95a5a6", relief='flat', height = 5, width = 10).place(x=720,y=180)
+        #A label for the top header
+        tk.Label(self.MainWindow, text = " "
+                     , font = (self.MainFont,14), background = '#861111', fg="#ffffff", height = 2, width = 200).place(x=0, y=0)
+        #The contents of the top header
+        #Icon
+        self.banner = ImageTk.PhotoImage(file='images/dotaspyiconsm.png')
+        tk.Label(self.MainWindow, background = "#861111", image = self.banner, width=40, height=40).place(x=0, y=0)
+        #Link text
         tk.Label(self.MainWindow, text = "Steam Profile Link: "
-                     , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=0, y=2)
-        self.SteamIDEntry = tk.Entry(self.MainWindow, bd = 0, font = (self.MainFont,12), exportselection=0, textvariable = self.steamURL, width=40).place(x=155,y=5)
-        tk.Button(self.MainWindow, text ="Submit", command= self.getData, bg= "#95a5a6", relief='flat').place(x=485,y=2)
+                     , font = (self.MainFont,14), background = '#861111', fg="#ffffff").place(x=50, y=8)
+        #Entry field and button for SteamID
+        self.steamURL = tk.StringVar()
+        self.SteamIDEntry = tk.Entry(self.MainWindow, bd = 0, font = (self.MainFont,12), exportselection=0, textvariable = self.steamURL, width=40).place(x=205,y=11)
+        tk.Button(self.MainWindow, text ="Submit", command= self.getData, bg= "#95a5a6", relief='flat').place(x=535,y=8)
+        #Matches to get entry
         tk.Label(self.MainWindow, text = "Scan last X games: "
-                     , font = (self.MainFont,14), background = self.WindowBackgroundColor, fg="#ffffff").place(x=550, y=2)
+                     , font = (self.MainFont,14), background = '#861111', fg="#ffffff").place(x=600, y=8)
         self.MatchesToGet = tk.IntVar()
         self.MatchesToGet.set(50)
-        self.MatchesToGetEntry = tk.Entry(self.MainWindow, bd = 0, font = (self.MainFont,12), exportselection=0, textvariable = self.MatchesToGet, width=4).place(x = 700,y=5)
+        self.MatchesToGetEntry = tk.Entry(self.MainWindow, bd = 0, font = (self.MainFont,12), exportselection=0, textvariable = self.MatchesToGet, width=4).place(x = 750,y=11)
 
 #Look for api key file
 #If exist
---
+++ readme.md
@@ -0,0 +1,18 @@
+#DotaSpy
+
+DotaSpy is an application that allows a user to gather information about a Dota 2 player using there Steam Profile.
+All they have to do is paste the steam profile link in, submit it and they're given the players name, picture, role and most played 3 heroes.
+
+This information is gotten by asking the Steam and Dota API's for information about the players last X games, where X defaults at 50 and can be changed by the user, from 1-100.
+
+This project was written in Python and uses the following libraries:
+Tkinter
+os
+webbrowser 
+urllib (Request)
+json
+io
+PIL (Image, ImageTK)
+
+PIL is Pillow in this case. The program wont run properly unless you have this installed.
+To install, follow guide at https://pillow.readthedocs.org/en/3.0.0/installation.html
